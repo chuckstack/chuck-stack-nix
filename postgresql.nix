24fa89f5e8d89c2d6122fc2a1f44801906a6025c
Chuck Boecking <chuck@chuboe.com>
2024-10-07 16:29:32 -0500

    postgresql.nix - much better defaults and comments

--- postgresql.nix
+++ postgresql.nix
@@ -8,10 +8,46 @@ services.postgresql = {
      enable = true;
      enableTCPIP = true;
      authentication = pkgs.lib.mkOverride 10 ''
+ # allow local OS users to connect if user exists on the db
  local all       all     peer
- host all all      ::1/128      scram-sha-256
- host all postgres 127.0.0.1/32 scram-sha-256
+ 
+ # allow local connections via ipv4 - uncomment the following line if needed
+ # host all all      127.0.0.1/32      scram-sha-256
+ 
+ # allow local connections via ipv6 - uncomment the following line if needed
+ # host all all      ::1/128      scram-sha-256
+ 
+ # allow local connections via ipv4 for postgres user - uncomment the following line if needed
+ # host all postgres 127.0.0.1/32 scram-sha-256
+
+ # all remote ipv4 to connect - uncomment the following line if needed
+ # host   all     all     0.0.0.0/0       scram-sha-256
+
+ # all remote ipv6 to connect - uncomment the following line if needed
+ # host   all     all     ::/0       scram-sha-256
      '';
    };
 }
-# TODO: update for unix sockets
+
+# the above configures all local users (with a user on the db) to connect using psql
+# the below notes gives instructions on how to add root as a db user so they can use the default postgres db for quick testing
+
+# from sudo -u postgres psql
+#    create role root with login;
+#    GRANT CONNECT ON DATABASE postgres TO root;
+#    GRANT USAGE ON SCHEMA public TO root;
+#    GRANT create ON SCHEMA public TO root;
+#    GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO root;
+#    GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO root;
+#    ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO root;
+#    ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO root;
+#    ALTER USER root SET search_path TO public;
+
+# in cli:
+#    echo "export PGDATABASE=postgres" >> ~/.bashrc
+#    #restart the shell then type: psql
+
+# disable user
+#    ALTER USER root NOLOGIN;
+# enable user
+#    ALTER USER root LOGIN;
