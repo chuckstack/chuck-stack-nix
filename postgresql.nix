f4f2f639999bc431e18067682e7aabdc935b0a26
Chuck Boecking <chuck@chuboe.com>
2024-10-09 09:48:11 -0500

    added sqlx-cli to postgresql - fix type-o

--- postgresql.nix
+++ postgresql.nix
@@ -1,37 +1,37 @@
 { config, lib, pkgs, modulesPath, ... }:
 
-environment.systemPackages = with pkgs; [
-  # other packages...
-  sqlx-cli
-];
-
 {
-services.postgresql = {
+  environment.systemPackages = with pkgs; [
+    # other packages...
+    sqlx-cli
+  ];
+  
+  services.postgresql = {
      settings = {
        listen_addresses = "*";
      };
      enable = true;
      enableTCPIP = true;
      authentication = pkgs.lib.mkOverride 10 ''
- # allow local OS users to connect if user exists on the db
- local all       all     peer
- 
- # allow local connections via ipv4 - uncomment the following line if needed
- # host all all      127.0.0.1/32      scram-sha-256
- 
- # allow local connections via ipv6 - uncomment the following line if needed
- # host all all      ::1/128      scram-sha-256
- 
- # allow local connections via ipv4 for postgres user - uncomment the following line if needed
- # host all postgres 127.0.0.1/32 scram-sha-256
+   # allow local OS users to connect if user exists on the db
+   local all       all     peer
+   
+   # allow local connections via ipv4 - uncomment the following line if needed
+   # host all all      127.0.0.1/32      scram-sha-256
+   
+   # allow local connections via ipv6 - uncomment the following line if needed
+   # host all all      ::1/128      scram-sha-256
+   
+   # allow local connections via ipv4 for postgres user - uncomment the following line if needed
+   # host all postgres 127.0.0.1/32 scram-sha-256
 
- # all remote ipv4 to connect - uncomment the following line if needed
- # host   all     all     0.0.0.0/0       scram-sha-256
+   # all remote ipv4 to connect - uncomment the following line if needed
+   # host   all     all     0.0.0.0/0       scram-sha-256
 
- # all remote ipv6 to connect - uncomment the following line if needed
- # host   all     all     ::/0       scram-sha-256
+   # all remote ipv6 to connect - uncomment the following line if needed
+   # host   all     all     ::/0       scram-sha-256
      '';
-   };
+  };
 }
 
 # the above configures all local users (with a user on the db) to connect using psql
