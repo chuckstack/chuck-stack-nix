e2c6eb3a7ef27345ac2cc93da8f18f6f5fd55804
Chuck Boecking <chuck@chuboe.com>
2024-10-14 16:39:58 -0500

    added users.nix - minor chantes

--- users.nix
+++ users.nix
@@ -17,9 +17,12 @@
       isSystemUser = true;
       group = "serviceuser";
       description = "User for running services";
+
+      # uncomment these lines if you need the user to have a home
+      #home = "/var/lib/serviceuser";
+      #createHome = true;
+      #shell = pkgs.bashInteractive;  # or pkgs.nologin if you want to prevent interactive login
+
     };
   };
-
-  # Create a group for the service user
-  users.groups.serviceuser = {};
 }
