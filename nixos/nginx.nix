98491cf5f88340c305bd5ee7bc7152f1f31cbd08
Chuck Boecking <chuck@chuboe.com>
2024-10-23 15:04:34 -0500

    added nginx-fail2ban v2

--- nixos/nginx.nix
+++ nixos/nginx.nix
@@ -33,6 +33,12 @@
         };
       };
     };
+
+    # Enable access and error logging
+    appendHttpConfig = ''
+      access_log /var/log/nginx/access.log;
+      error_log /var/log/nginx/error.log;
+    '';
   };
 
   # staging server used for testing
